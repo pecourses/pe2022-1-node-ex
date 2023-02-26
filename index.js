2ad43007af39b184020172203982644f41a2ecdf
Viktoriia <pecourses>
2023-02-26 17:24:27 +0200

    Add index.html

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
+const { readFile } = require('fs/promises');
 const http = require('http');
 
 const httpServer = http.createServer((request, response) => {
   console.log('url :>> ', request.url);
   console.log('method :>> ', request.method);
 
-  response.statusCode = 200;
-  response.setHeader('Content-Type', 'text/html');
-  response.end('result');
+  readFile('./index.html', { encoding: 'utf-8' })
+    .then(data => {
+      response.statusCode = 200;
+      response.setHeader('Content-Type', 'text/html');
+      response.end(data);
+    })
+    .catch(err => {
+      response.statusCode = 500;
+      response.setHeader('Content-Type', 'text/html');
+      response.end('file was not read');
+    });
 });
 
 httpServer.listen(5000, 'localhost', () => {
